561230c7df8be97fb40cf14e617604cde81df3f8
mlcoding111 <kennycodes@hotmail.com>
2021-10-12 12:03:12 -0400

    Added basic server setup for mongoDB and client file structure

--- server/index.js
+++ server/index.js
@@ -1,12 +1,29 @@
-import express, { application } from 'express';
+// https://youtu.be/ngc9gnGgUdA?t=1248
+
+import dotenv from 'dotenv'
+dotenv.config()
+
+import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
 const app = express();
+
+// Routes
+import postRoutes from './routes/posts.js'
+
+const USER = process.env.USER
+const PASS = process.env.PASSWORD
+
+app.use('/posts', postRoutes)
 
 app.use(express.json({limit: "20mb", extended:true}));
 app.use(express.urlencoded({ extended: true}))
-
 app.use(cors());
 
-const CONNECTION_URL = "mongodb+srv://MLWeb:<password>@pgs.qkqxy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const CONNECTION_URL = `mongodb+srv://${USER}:${PASS}@pgs.qkqxy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(()=> app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
+    .catch((error)=> console.log(error.message))
+
