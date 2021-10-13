33ab3bf2ebb19934bb3ee8bd56b0e12818bc7e37
mlcoding111 <kennycodes@hotmail.com>
2021-10-13 14:17:40 -0400

    Added postMessage Schema and Controllers for the backend

--- server/index.js
+++ server/index.js
@@ -1,28 +1,28 @@
-// https://youtu.be/ngc9gnGgUdA?t=1248
-
 import dotenv from 'dotenv'
 dotenv.config()
-
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 const app = express();
 
-// Routes
-import postRoutes from './routes/posts.js'
-
+// Variables
 const USER = process.env.USER
 const PASS = process.env.PASSWORD
+const CONNECTION_URL = `mongodb+srv://${USER}:${PASS}@pgs.qkqxy.mongodb.net/datas?retryWrites=true&w=majority`
+const PORT = process.env.PORT || 5000;
 
+// Routes import
+import postRoutes from './routes/posts.js'
+
+//Routes 
 app.use('/posts', postRoutes)
 
 app.use(express.json({limit: "20mb", extended:true}));
 app.use(express.urlencoded({ extended: true}))
 app.use(cors());
 
-const CONNECTION_URL = `mongodb+srv://${USER}:${PASS}@pgs.qkqxy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
-const PORT = process.env.PORT || 5000;
 
+// Connect to database
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(()=> app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
     .catch((error)=> console.log(error.message))
