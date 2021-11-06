4945431f0b5ba28ccbad4c7fce45d1b54407e714
mlcoding111 <kennycodes@hotmail.com>
2021-11-06 11:00:08 -0400

    Added api functions for each section

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -9,4 +9,16 @@ export const createPost = (newPost) => axios.post(`${url}posts`, newPost)
 
 // Dossiers
 export const fetchDossiers = () => axios.get(`${url}dossiers`)
-export const createDossier = (newDossier) => axios.post(`${url}posts`, newDossier)
+export const createDossier = (newDossier) => axios.post(`${url}dossiers`, newDossier)
+
+// Facturations
+export const fetchFacturations = () => axios.get(`${url}facturations`)
+export const createFacturation = (newFacturation) => axios.post(`${url}facturations`, newFacturation)
+
+// Paiements
+export const fetchPaiements = () => axios.get(`${url}paiements`)
+export const createPaiement = (newPaiement) => axios.post(`${url}paiements`, newPaiement)
+
+// Employers
+export const fetchEmployers = () => axios.get(`${url}employers`)
+export const createEmployer = (newEmployer) => axios.post(`${url}employers`, newEmployer)
